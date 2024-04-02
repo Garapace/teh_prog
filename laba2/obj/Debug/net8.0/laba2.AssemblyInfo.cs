63b6df7262be21d7ad1a3fc9e757714a3f5c0977
Garapace <starkov-2003@list.ru>
2024-04-03 00:21:12 +0500

    тест

--- laba2/obj/Debug/net8.0/laba2.AssemblyInfo.cs
+++ laba2/obj/Debug/net8.0/laba2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3ad94004f8f8a015b221ba181213b53eab802b")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
